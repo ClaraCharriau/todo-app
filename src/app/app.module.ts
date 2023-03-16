996c95e40ced885677d3d9c574fea15fcf60d018
Clara Charriau <charriau.clara@gmail.com>
2023-03-16 12:08:14 +0100

    [FEATURE] Creation of main components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './pages/home/home.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { TaskCreationComponent } from './pages/task-creation/task-creation.component';
+import { HistoryComponent } from './pages/history/history.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    NavbarComponent,
+    TaskCreationComponent,
+    HistoryComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/pages/task-creation/task-creation.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-task-creation',
+  templateUrl: './task-creation.component.html',
+  styleUrls: ['./task-creation.component.css']
+})
+export class TaskCreationComponent {
+
+}
