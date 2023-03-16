4503b98cf6d8c4ad5527020483e62ba1091665e9
Clara Charriau <charriau.clara@gmail.com>
2023-03-16 16:25:33 +0100

    [CHORE] correction typo

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { TaskService } from 'src/app/services/task/task.service';
+import { Task } from 'src/app/task';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +8,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+
+  todoList: Task[] = [];
+
+  constructor(private taskService: TaskService) {}
+
+  ngOnInit() {
+    this.todoList = this.taskService.getToDos();
+  }
+
 
 }
