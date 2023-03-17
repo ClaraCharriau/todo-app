f5df6a3447008178761a58a466001518f87e39df
Clara Charriau <charriau.clara@gmail.com>
2023-03-17 15:17:44 +0100

    [STYLE] style history page

---
+++ src/app/pipes/category-emoji.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { CategoryEmojiPipe } from './category-emoji.pipe';
+
+describe('CategoryEmojiPipe', () => {
+  it('create an instance', () => {
+    const pipe = new CategoryEmojiPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
---
+++ src/app/pipes/category-emoji.pipe.ts
@@ -0,0 +1,37 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { CategoryType } from '../task';
+
+@Pipe({
+  name: 'categoryEmoji'
+})
+export class CategoryEmojiPipe implements PipeTransform {
+
+  transform(category: CategoryType): string | undefined {
+    let emoji: string | undefined;
+    switch(category) {
+      case 'shopping':
+        emoji = "🛍️";
+        break;
+      case 'health':
+        emoji = "💊";
+        break;
+      case 'work':
+        emoji = "💼";
+        break;
+      case 'bills':
+        emoji = "💸";
+        break;
+      case 'cleaning':
+        emoji = "🧼";
+        break;
+      case 'other':
+        emoji = "🤷‍♀️";
+        break;
+      default:
+        emoji = undefined;
+        break;
+    }
+    return emoji === undefined ? undefined : emoji;
+  }
+
+}
